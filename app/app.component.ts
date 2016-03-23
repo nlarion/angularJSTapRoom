45491123be0b32f23e6830373d6af17c6ff4ac0a
michael smith and neil larion <michaelanthonysmith00@gmail.com>
2016-03-23 09:36:54 -0700

    update app component to display one single keg object.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
 
 @Component({
   selector: 'my-app',
   template:`
     <div class="container">
-      <h1>Skeleton Angular2 App</h1>
+      <h1>THE BAR</h1>
+      <h3>{{ keg.name }} {{ keg.brand }} $ {{ keg.price }} {{ keg.alcoholContent }} {{ keg.pints }} </h3>
     </div>
   `
 })
 
 export class AppComponent{
-
+  public keg: Keg;
+  constructor(){
+    this.keg = new Keg("Bud Light", "Anheisher Busch", 3, "4%", 124, 0);
+  }
 }
