ccbe74c9f2de7b189252e41e320ab067bbde4b1d
michael smith and neil larion <michaelanthonysmith00@gmail.com>
2016-03-23 13:50:19 -0700

    add a button to decrement the amount of pints left in a keg by 1.

--- app/edit-keg-details.components.ts
+++ app/edit-keg-details.components.ts
@@ -1,12 +1,26 @@
-import {Component} from 'angular2/core';
+import {Component, EventEmitter } from 'angular2/core';
 import {Keg} from './keg.model';
 
 @Component({
   selector: 'edit-keg-details',
   inputs: ['keg'],
   template: `
-  <h3>Edit Name:</h3>
-  <input [(ngModel)]="keg.name" class="col-sm-8 input-lg keg-form"/>
+  <div class="row">
+    <h3>Edit Name:</h3>
+    <input [(ngModel)]="keg.name" class="col-sm-8 input-lg keg-form"/>
+  </div>
+  <div class="row">
+    <h3>Edit brand:</h3>
+    <input [(ngModel)]="keg.brand" class="col-sm-8 input-lg keg-form"/>
+  </div>
+  <div class="row">
+    <h3>Edit Price:</h3>
+    <input [(ngModel)]="keg.price" class="col-sm-8 input-lg keg-form"/>
+  </div>
+  <br>
+  <div class="row">
+    <button (click)="keg.sellPint()" class="btn btn-danger">Sell</button>
+  </div>
   `
 })
 export class EditKegDetailsComponent {
--- app/keg.model.ts
+++ app/keg.model.ts
@@ -1,5 +1,7 @@
 export class Keg {
   constructor(public name: string, public brand: string, public price: number, public alcoholContent: string, public pints: number, public id: number){
-
+  }
+  sellPint(): void {
+    this.pints = this.pints -1;
   }
 }
